588885f77725416ca97159e1978d54c9a95cc8ae
Filippo Fiorenza <fiorenza@stud.hs-heilbronn.de>
2023-04-06 11:30:41 +0200

    update SliderTemp

--- heinzprog23/src/SliderTemp.java
+++ heinzprog23/src/SliderTemp.java
@@ -1,3 +1,4 @@
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import javax.swing.JFrame;
 import javax.swing.JSlider;
@@ -8,15 +9,38 @@ import javax.swing.event.ChangeListener;
 
 public class SliderTemp extends JFrame implements ChangeListener {
 
-  JSlider celsiusSlider, fahrenheitSlider;
-  JTextField degreeCelsius, degreeFahrenheit;
+  JSlider celsiusSlider;
+  JSlider fahrenheitSlider;
+  JTextField degreeCelsius;
+  JTextField degreeFahrenheit;
 
   public SliderTemp() {
     setTitle("Temperature Converter");
     setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-    celsiusSlider = new JSlider(SwingConstants.HORIZONTAL, -100, 100,20);
-    fahrenheitSlider = new JSlider(SwingConstants.HORIZONTAL, -20, 150, 40);
+    celsiusSlider = new JSlider(SwingConstants.HORIZONTAL, -100, 100, 20);
+    celsiusSlider.setMajorTickSpacing(20);
+    celsiusSlider.setMinorTickSpacing(50);
+    celsiusSlider.setPaintTicks(true);
+    celsiusSlider.setPaintLabels(true);
+    celsiusSlider.setPreferredSize(new Dimension(300, 50));
+    celsiusSlider.addChangeListener(this);
+    celsiusSlider.setName("celsiusSlider");
+
+    degreeCelsius = new JTextField(4);
+    degreeCelsius.setText(celsiusSlider.getValue() + " ");
+
+    fahrenheitSlider = new JSlider(SwingConstants.HORIZONTAL, -20, 140, 40);
+    fahrenheitSlider.setMajorTickSpacing(20);
+    fahrenheitSlider.setMinorTickSpacing(50);
+    fahrenheitSlider.setPaintTicks(true);
+    fahrenheitSlider.setPaintLabels(true);
+    fahrenheitSlider.setPreferredSize(new Dimension(300, 50));
+    fahrenheitSlider.addChangeListener(this);
+    fahrenheitSlider.setName("fahrenheitSlider");
+
+    degreeFahrenheit = new JTextField(4);
+    degreeFahrenheit.setText(fahrenheitSlider.getValue() + " ");
 
     getContentPane().setLayout(new FlowLayout());
     getContentPane().add(celsiusSlider);
@@ -27,15 +51,26 @@ public class SliderTemp extends JFrame implements ChangeListener {
     degreeFahrenheit.setEditable(false);
   }
 
-  @Override
-  public void stateChanged(ChangeEvent e) {
-
-  }
-
-  public static void main(String[] args){
+  public static void main(String[] args) {
     SliderTemp tempConverter = new SliderTemp();
     tempConverter.setSize(400, 150);
     tempConverter.setResizable(false);
     tempConverter.setVisible(true);
+  }
+
+  @Override
+  public void stateChanged(ChangeEvent e) {
+    JSlider data;
+    data = (JSlider) e.getSource();
+    if (!data.getValueIsAdjusting() && data.getName().equals("celsiusSlider")) {
+      degreeCelsius.setText(data.getValue() + " ");
+      degreeFahrenheit.setText(data.getValue() * 1.8 + 32 + " ");
+      fahrenheitSlider.setValue((int) (data.getValue() * 1.8 + 32));
+    }
+    if (data.getName().equals("fahrenheitSlider")) {
+      degreeFahrenheit.setText(data.getValue() + " ");
+      degreeCelsius.setText((data.getValue() - 32) * 5 / 9 + " ");
+      celsiusSlider.setValue((data.getValue() - 32) * 5 / 9);
+    }
   }
 }
