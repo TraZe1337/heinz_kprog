a004e2605dc17e2ab44e6685b3c89eb3584532d6
Stefan Janke <stefanj.1337@gmx.de>
2023-06-10 22:10:22 +0200

    menu bar

--- heinzprog23/src/GameOfLife/EnvironmentView.java
+++ heinzprog23/src/GameOfLife/EnvironmentView.java
@@ -2,17 +2,16 @@ package GameOfLife;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
-import javax.swing.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * A GUI for the environment, with runtime controls.
- * 
+ *
  * @author David J. Barnes and Michael Kölling
- * @version  2016.02.29
+ * @version 2016.02.29
  */
-public class EnvironmentView extends JFrame
-{
+public class EnvironmentView extends JFrame {
 
 
     public static void main(String[] args) {
@@ -33,52 +32,42 @@ public class EnvironmentView extends JFrame
     private static final int LONGEST_DELAY = 1000;
     // Colors for the different cell states.
     private static final Color[] colors = {
-        new Color(252, 10, 10), // Alive
-        new Color(23, 227, 0), // Dead
-        new Color(252, 10, 10),  // Dying
+            new Color(252, 10, 10), // Alive
+            new Color(23, 227, 0), // Dead
+            new Color(252, 10, 10),  // Dying
     };
-    private static int windowcounter=-1;
+    private static int windowcounter = 0;
     private GridView view;
     private final Environment env;
     private boolean running;
     private int delay;
 
-    JMenu[] menus = { new JMenu("Modus"), new JMenu("Geschwindigkeit"), new JMenu("Fenster"), new JMenu("Figuren") };
-    JMenuItem[] items = {
+    private JMenu[] menus = {new JMenu("Modus"), new JMenu("Geschwindigkeit"), new JMenu("Fenster"), new JMenu("Figuren")};
+    private JMenuItem[] items = {
             new JMenuItem("Laufen"), new JMenuItem("Pause"),
-            new JMenuItem("Reset"), new JMenuItem("Beenden"),
-            new JMenuItem("Rückgängig"), new JMenuItem("Widerrufen"),
-            new JMenuItem("Ausschneiden"), new JMenuItem("Kopieren"),
-            new JMenuItem("Einfügen"), new JMenuItem("Suchen"),
-            new JMenuItem("Ersetzen") };
+            new JMenuItem("Reset"), new JMenuItem("Random"),
+            new JMenuItem("Schneller"), new JMenuItem("Langsamer"),
+            new JMenuItem("Clone"), new JMenuItem("Gleiter"),
+            new JMenuItem("Tümmler"), new JMenuItem("Oktagon"),
+            new JMenuItem("Segler1"), new JMenuItem("Segler2"),
+            new JMenuItem("Segler3"),
+    };
 
     ActionListener al = new ActionListener() { // AL als anonyme Klasse
-        public void actionPerformed(ActionEvent e){ // ... fuer alle MenuItems
+        public void actionPerformed(ActionEvent e) { // ... fuer alle MenuItems
         }
     };
 
     /**
      * Constructor for objects of class EnvironmentView
+     *
      * @param env
      */
-    public EnvironmentView(Environment env, int rows, int cols)
-    {
+    public EnvironmentView(Environment env, int rows, int cols) {
         super("Game of Life");
         windowcounter++;
         setTitle("Game of Life - Fenster " + windowcounter);
-        menus[0].setToolTipText("Operationen auf Dateien");
-        items[4].setToolTipText("Nimm letzten Befehl zurück");
-        for (int i = 0; i < items.length; i++) {
-            items[i].addActionListener(al);
-            menus[(i<4)?0:(i<9)?1:2].add(items[i]);
-        }
-        JMenuBar mb = new JMenuBar();
-        for (int i = 0; i < menus.length; i++)
-            mb.add (menus[i]); //
-        setJMenuBar (mb);
-        Container cp = getContentPane();
-        cp.setLayout(new FlowLayout());
-
+        setupMenu();
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocation(20, 20);
         this.env = env;
@@ -92,57 +81,65 @@ public class EnvironmentView extends JFrame
 
     /**
      * Setup a new environment of the given size.
+     *
      * @param rows The number of rows.
      * @param cols The number of cols;
      */
-    private void setupGrid(int rows, int cols)
-    {
+    private void setupGrid(int rows, int cols) {
         Container contents = getContentPane();
         view = new GridView(rows, cols);
         contents.add(view, BorderLayout.CENTER);
     }
-    
+
+    private void setupMenu() {
+        for (int i = 0; i < items.length; i++) {
+            menus[(i < 4) ? 0 : (i < 6) ? 1 : (i < 7) ? 2 : (i < 13) ? 3 : 4].add(items[i]); // if Teil -> (i<4), ? -> true Teil, : -> false Teil
+        }
+        JMenuBar mb = new JMenuBar();
+        for (int i = 0; i < menus.length; i++)
+            mb.add(menus[i]); //
+        setJMenuBar(mb);
+        Container cp = getContentPane();
+        cp.setLayout(new FlowLayout());
+    }
+
     /**
      * Show the states of the cells.
      */
-    public void showCells()
-    {
+    public void showCells() {
         Cell[][] cells = env.getCells();
-        if(!isVisible()) {
+        if (!isVisible()) {
             setVisible(true);
         }
-        
+
         view.preparePaint();
-        for(int row = 0; row < cells.length; row++) {
+        for (int row = 0; row < cells.length; row++) {
             Cell[] cellRow = cells[row];
             int numCols = cellRow.length;
-            for(int col = 0; col < numCols; col++) {
+            for (int col = 0; col < numCols; col++) {
                 int state = cellRow[col].getState();
                 view.drawMark(col, row, colors[state]);
             }
         }
-        
+
         view.repaint();
     }
-    
+
     /**
      * Set up the animation controls.
      */
-    private void setupControls()
-    {
+    private void setupControls() {
         // Continuous running.
-        final JButton run = new JButton("Laufen");
-        run.addActionListener(e -> {
-            if(!running) {
+        items[0].addActionListener(e -> {
+            if (!running) {
                 running = true;
                 try {
                     new Runner().execute();
-                }
-                catch(Exception ex) {
+                } catch (Exception ex) {
                 }
             }
         });
-        
+
         // Single stepping.
         final JButton step = new JButton("Step");
         step.addActionListener(e -> {
@@ -152,28 +149,23 @@ public class EnvironmentView extends JFrame
         });
 
         //neues Fenster
-        final JButton neuesFenster = new JButton("neues Fenster öffnen");
-        neuesFenster.addActionListener(e -> {
+        items[6].addActionListener(e -> {
             Environment clone = new Environment();
-
         });
 
 
         // Pause the animation.
-        final JButton pause = new JButton("Pause");
-        pause.addActionListener(e -> running = false);
-        
+        items[1].addActionListener(e -> running = false);
+
         // Reset of the environment
-        final JButton reset = new JButton("Reset");
-        reset.addActionListener(e -> {
+        items[2].addActionListener(e -> {
             running = false;
             env.reset();
             showCells();
         });
-        
+
         // Randomize the environment.
-        final JButton randomize = new JButton("Random");
-        randomize.addActionListener(e -> {
+        items[3].addActionListener(e -> {
             running = false;
             env.randomize();
             showCells();
@@ -186,62 +178,53 @@ public class EnvironmentView extends JFrame
             env.set();
             showCells();
         });
-        
+
         Container contents = getContentPane();
-        
-        // A speed controller.
-        final JSlider speedSlider = new JSlider(0, 100);
-        speedSlider.addChangeListener(e -> {
-            setDelay(speedSlider.getValue());
+        // More Speed
+        items[4].addActionListener(e -> {
+            delay -= 100;
         });
-        Container speedPanel = new JPanel();
-        speedPanel.setLayout(new GridLayout(2, 1));
-        speedPanel.add(new JLabel("Animation Speed", SwingConstants.CENTER));
-        speedPanel.add(speedSlider);
-        contents.add(speedPanel, BorderLayout.NORTH);
-        
+
+        //Less Speed
+        items[5].addActionListener(e -> {
+            delay += 100;
+        });
+
         // Place the button controls.
         JPanel controls = new JPanel();
-        controls.add(run);
         controls.add(step);
-        controls.add(pause);
-        controls.add(reset);
-        controls.add(randomize);
         controls.add(setzen);
-        controls.add(neuesFenster);
-        
+
+
         contents.add(controls, BorderLayout.SOUTH);
     }
 
-    
+
     /**
      * Set the animation delay.
+     *
      * @param speedPercentage (100-speedPercentage) as a percentage of the LONGEST_DELAY.
      */
-    private void setDelay(int speedPercentage)
-    {
+    private void setDelay(int speedPercentage) {
         delay = (int) ((100.0 - speedPercentage) * LONGEST_DELAY / 100);
     }
-    
+
     /**
      * Provide stepping of the animation.
      */
-    private class Runner extends SwingWorker<Boolean, Void>
-    {
+    private class Runner extends SwingWorker<Boolean, Void> {
         @Override
         /**
          * Repeatedly single-step the environment as long
          * as the animation is running.
          */
-        public Boolean doInBackground()
-        {
-            while(running) {
+        public Boolean doInBackground() {
+            while (running) {
                 env.step();
                 showCells();
                 try {
                     Thread.sleep(delay);
-                }
-                catch(InterruptedException e) {
+                } catch (InterruptedException e) {
                 }
             }
             return true;
@@ -253,8 +236,7 @@ public class EnvironmentView extends JFrame
      * Provide a graphical view of a rectangular grid.
      */
     @SuppressWarnings("serial")
-    private class GridView extends JPanel
-    {
+    private class GridView extends JPanel {
         private final int GRID_VIEW_SCALING_FACTOR = 10;
 
         private final int gridWidth, gridHeight;
@@ -266,8 +248,7 @@ public class EnvironmentView extends JFrame
         /**
          * Create a new GridView component.
          */
-        public GridView(int height, int width)
-        {
+        public GridView(int height, int width) {
             gridHeight = height;
             gridWidth = width;
             size = new Dimension(0, 0);
@@ -277,41 +258,38 @@ public class EnvironmentView extends JFrame
          * Tell the GUI manager how big we would like to be.
          */
         @Override
-        public Dimension getPreferredSize()
-        {
+        public Dimension getPreferredSize() {
             return new Dimension(gridWidth * GRID_VIEW_SCALING_FACTOR,
-                                 gridHeight * GRID_VIEW_SCALING_FACTOR);
+                    gridHeight * GRID_VIEW_SCALING_FACTOR);
         }
 
         /**
          * Prepare for a new round of painting. Since the component
          * may be resized, compute the scaling factor again.
          */
-        public void preparePaint()
-        {
-            if(! size.equals(getSize())) {
+        public void preparePaint() {
+            if (!size.equals(getSize())) {
                 size = getSize();
                 fieldImage = view.createImage(size.width, size.height);
                 g = fieldImage.getGraphics();
 
                 xScale = size.width / gridWidth;
-                if(xScale < 1) {
+                if (xScale < 1) {
                     xScale = GRID_VIEW_SCALING_FACTOR;
                 }
                 yScale = size.height / gridHeight;
-                if(yScale < 1) {
+                if (yScale < 1) {
                     yScale = GRID_VIEW_SCALING_FACTOR;
                 }
             }
         }
-        
+
         /**
          * Paint on grid location on this field in a given color.
          */
-        public void drawMark(int x, int y, Color color)
-        {
+        public void drawMark(int x, int y, Color color) {
             g.setColor(color);
-            g.fillRect(x * xScale, y * yScale, xScale-1, yScale-1);
+            g.fillRect(x * xScale, y * yScale, xScale - 1, yScale - 1);
         }
 
         /**
@@ -319,14 +297,12 @@ public class EnvironmentView extends JFrame
          * internal image to screen.
          */
         @Override
-        public void paintComponent(Graphics g)
-        {
-            if(fieldImage != null) {
+        public void paintComponent(Graphics g) {
+            if (fieldImage != null) {
                 Dimension currentSize = getSize();
-                if(size.equals(currentSize)) {
+                if (size.equals(currentSize)) {
                     g.drawImage(fieldImage, 0, 0, null);
-                }
-                else {
+                } else {
                     // Rescale the previous image.
                     g.drawImage(fieldImage, 0, 0, currentSize.width, currentSize.height, null);
                 }
