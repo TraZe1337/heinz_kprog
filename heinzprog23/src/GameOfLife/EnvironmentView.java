23ffb64d37cfb12561fb967aaa8f16a53ddd966d
samilSEB <saoeztue@stud.hs-heilbronn.de>
2023-06-15 10:52:59 +0200

    main

--- heinzprog23/src/GameOfLife/EnvironmentView.java
+++ heinzprog23/src/GameOfLife/EnvironmentView.java
@@ -8,20 +8,6 @@ import java.awt.event.MouseMotionAdapter;
 
 public class EnvironmentView extends JFrame {
 
-    public static void main(String[] args) {
-        Environment environment = new Environment();
-        //TODO: counter in title rein - Samil //done
-        //TODO: Thread -> mehrere Instanzen - Samil // done
-        //TODO: Farben ändern (ROT/Grün) - Stefan // done
-        //TODO: Titel ändern - Stefan // done
-        //TODO: Run -> Laufen - Stefan // done
-        //TODO: Zellen Tot zu lebendig machen mit Klick - Stefan // done
-        //TODO: Modus Setzen: Zellenstatus switchen - Stefan // done
-        //TODO: Modus Malen: sollen alle Zellen bei Überstreichen mit der Maus lebendig werden. - Filippo
-        //TODO: Pop-up: Farben ändern - Tarik // done
-        //TODO: Muster (Gleiter, ...) - Stefan // done
-    }
-
     // The longest delay for the animation, in milliseconds.
     private static final int LONGEST_DELAY = 1000;
 
---
+++ heinzprog23/src/GameOfLife/run.java
@@ -0,0 +1,17 @@
+package GameOfLife;
+
+public class run {
+    public static void main(String[] args) {
+        Environment environment = new Environment();
+        //TODO: counter in title rein - Samil //done
+        //TODO: Thread -> mehrere Instanzen - Samil // done
+        //TODO: Farben ändern (ROT/Grün) - Stefan // done
+        //TODO: Titel ändern - Stefan // done
+        //TODO: Run -> Laufen - Stefan // done
+        //TODO: Zellen Tot zu lebendig machen mit Klick - Stefan // done
+        //TODO: Modus Setzen: Zellenstatus switchen - Stefan // done
+        //TODO: Modus Malen: sollen alle Zellen bei Überstreichen mit der Maus lebendig werden. - Filippo
+        //TODO: Pop-up: Farben ändern - Tarik // done
+        //TODO: Muster (Gleiter, ...) - Stefan // done
+    }
+}
